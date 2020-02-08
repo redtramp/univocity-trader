115732fb8af664e875e36e03a020df33b866e0c3
Jeronimo Backes <jbax@univocity.com>
2020-02-08 15:24:17 +0700

    adding test for account balances after filling order with multiple fills and multiple prices

--- univocity-trader-core/src/test/java/com/univocity/trader/account/TradingWithPartialFillTests.java
+++ univocity-trader-core/src/test/java/com/univocity/trader/account/TradingWithPartialFillTests.java
@@ -63,13 +63,22 @@ public class TradingWithPartialFillTests extends OrderFillChecker {
 		assertEquals(33.0, account.getBalance("ADA").getFree().doubleValue(), DELTA);
 		assertEquals(60.00404, account.getBalance("USDT").getFree().doubleValue(), DELTA);
 		assertEquals(39.99596, account.getBalance("USDT").getLocked().doubleValue(), DELTA);
+		assertEquals(6.956004, order.getRemainingQuantity().doubleValue(), DELTA);
 
-		tradeOnPrice(trader, 2, 0.5, NEUTRAL);
+		trader.tradingManager.updateOpenOrders(trader.symbol(), newTick(2, 0.5)); //next tick should fill 6.956004 units at $0.5
 
-		assertEquals(order.getQuantity().doubleValue(), order.getExecutedQuantity().doubleValue(), DELTA); //each tick has volume = 33 units
+		assertTrue(order.isFinalized());
+		assertEquals(100.0, order.getFillPct());
+
+		assertEquals(order.getQuantity().doubleValue(), order.getExecutedQuantity().doubleValue(), DELTA);
 		assertEquals(order.getQuantity().doubleValue(), account.getBalance("ADA").getFree().doubleValue(), DELTA);
 		assertEquals(0.0, account.getBalance("USDT").getLocked().doubleValue(), DELTA);
-		assertEquals(100.0, account.getBalance("USDT").getFree().doubleValue() + (33 * 1.0) + (6.99596 * 0.5) + feesOn(order.getTotalTraded()));
-		assertEquals(60.00404 + (6.99596 * 0.5), account.getBalance("USDT").getFree().doubleValue(), DELTA); //add amount not spent as price dropped in half
+		assertEquals(0.0, account.getBalance("ADA").getLocked().doubleValue(), DELTA);
+
+		double feesNotPaid = feesOn(order.getTotalOrderAmount().doubleValue()) - order.getFeesPaid().doubleValue();
+
+		//add amount not spent as price dropped in half
+		assertEquals(60.00404 + (6.956004 / 2.0) + feesNotPaid, account.getBalance("USDT").getFree().doubleValue(), 0.000001); //using less stringent DELTA here as calculation is correct but assertion fails due to rounding error
+		assertEquals(100.0, account.getBalance("USDT").getFree().doubleValue() + (33 * 1.0) + (6.956004 * 0.5) + order.getFeesPaid().doubleValue(), 0.000001);
 	}
 }
